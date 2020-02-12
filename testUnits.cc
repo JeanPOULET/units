e8c89255cb51c4378690f04b1b5389242993ad37
MathieuBart <mathieu.bart3@gmail.com>
2020-02-12 14:16:54 +0100

    oui

--- testUnits.cc
+++ testUnits.cc
@@ -4,6 +4,17 @@
 
 #include <gtest/gtest.h>
 
+TEST(tests,first){
+
+  using  namespace phy:: literals;
+  auto velocity = 100000_metres / 3600_seconds;
+  phy::Qty<phy::Metre , std::milli> mm(32);
+  auto nm = phy::qtyCast <phy::Qty <phy::Metre , std::nano >>(mm);
+  
+}
+
+
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
